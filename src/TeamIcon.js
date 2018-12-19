8f0a2e89e51f145dac210ddf9996b3e9779cdec1
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2018-12-18 18:41:49 -0700

    adds teamIcons to Container display

--- src/TeamIcon.js
+++ src/TeamIcon.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './reset.css';
+import './TeamIcon.scss';
 
 export default function TeamIcon(props) {
   return(
     <article>
-      <img src={props.nflTeam.helmet_image} /> 
+      <img src={props.nflTeam.team_icon} /> 
       <h2>{props.nflTeam.name}</h2>
     </article>
   )
