bb5eca01f41ec6e01e75e96c12a0a171f54fa8d7
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2018-12-19 17:46:05 -0700

    refactors for more readability

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import './Card.scss';
 
 export default class Card extends Component {
   constructor() {
     super();
     this.state = {
       teamInfo: true,
-      matchedCity: undefined
+      matchedCity: null
     }
   }
 
-
   findMatchingCity = () => {
+    const selectedTeam = this.props.nflTeams[this.props.clickedIndex];
     let matchingCity = this.props.cities.find((city) => {
-      return this.props.nflTeams[this.props.clickedIndex].city === city.name
+      return selectedTeam.city === city.name
     })
     this.setState({matchedCity: matchingCity})
   }
@@ -30,30 +31,34 @@ export default class Card extends Component {
 
   render() {
     const xIcon = 'X';
-
+    const selectedTeam = this.props.nflTeams[this.props.clickedIndex];
+    const selectedCity = this.state.matchedCity;
     if (this.state.teamInfo) {
       return (
         <div>
           <h1 onClick={this.props.toggleCardView}>{xIcon}</h1>
-          <img src={this.props.nflTeams[this.props.clickedIndex].team_img} />
-          <h2>{this.props.nflTeams[this.props.clickedIndex].name}</h2>
-          <h2>{this.props.nflTeams[this.props.clickedIndex].city}, {this.props.nflTeams[this.props.clickedIndex].state}</h2>
-          <h2>{this.props.nflTeams[this.props.clickedIndex].division}</h2>
+          <img src={selectedTeam.team_img} alt='image of NFL team'/>
+          <h2>{selectedTeam.name}</h2>
+          <h2>{selectedTeam.city}, {selectedTeam.state}</h2>
+          <h2>{selectedTeam.division}</h2>
           <button onClick={this.cityChange}>Show City Info</button>
         </div>
       )
     } else {
+      const nums = [0, 1, 2, 3];
+
+
       return (
         <div>
           <h1 onClick={this.props.toggleCardView}>{xIcon}</h1>
-          <img src={this.state.matchedCity.city_image} />
-          <h2>{this.state.matchedCity.name}</h2>
-          <h2>{this.state.matchedCity.state}</h2>
-          <h2>Population: {this.state.matchedCity.population}</h2>
-          <h2><a href={`https://google.com/search?q=${this.state.matchedCity.attractions[0].trim().split(' ').join('%20')}`} target="_blank" rel="noopener noreferrer" >{this.state.matchedCity.attractions[0]}</a></h2>
-          <h2><a href={`https://google.com/search?q=${this.state.matchedCity.attractions[1].trim().split(' ').join('%20')}`} target="_blank" rel="noopener noreferrer" >{this.state.matchedCity.attractions[1]}</a></h2>
-          <h2><a href={`https://google.com/search?q=${this.state.matchedCity.attractions[2].trim().split(' ').join('%20')}`} target="_blank" rel="noopener noreferrer" >{this.state.matchedCity.attractions[2]}</a></h2>
-          <h2><a href={`https://google.com/search?q=${this.state.matchedCity.attractions[3].trim().split(' ').join('%20')}`} target="_blank" rel="noopener noreferrer" >{this.state.matchedCity.attractions[3]}</a></h2 >
+          <img src={selectedCity.city_image} alt="image of city" />
+          <h2>{selectedCity.name}, {selectedCity.state}</h2>
+          <h2>Population: {selectedCity.population}</h2>
+      {nums.map((number) => {
+        let link = `https://google.com/search?q=${selectedCity.attractions[`${number}`].trim().split(' ').join('%20')}`
+        return  <h2><a href={link} target="_blank" rel="noopener noreferrer" >{selectedCity.attractions[`${number}`]}</a></h2>
+      })}
+        
           <button onClick={this.toggleCityInfo}>Show Team Info</button>
         </div>
       )
