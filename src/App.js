194a1c7f32462d6ea317d5406fbc90afe205dd6a
B3an5 <beantheo@gmail.com>
2018-12-19 19:19:27 -0700

    added loading screen gif

--- src/App.js
+++ src/App.js
@@ -5,18 +5,48 @@ import Container from './Container.js'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nflTeams: null,
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://whateverly-datasets.herokuapp.com/api/v1/nflTeams')
+      .then(nflTeams => nflTeams.json())
+      .then(data => {
+        setTimeout(() => {
+          this.setState({
+            nflTeams: data.nflTeams,
+            isLoading: false
+          })
+        }, 1000);
+      }
+      )
+      .catch(error => console.log(error))
+  }
+
   render() {
-    return (
-      <div className="app">
-        <div className='app-title-area'>
-          <h1 className='app-title'>The Traveling Fan</h1>
+    if(this.state.isLoading) {
+      return(
+        <div className="loading-screen"><img className="loading-image" src='https://media.giphy.com/media/lXiRCXQ2OWXO3xPr2/giphy.gif' /></div>
+      )
+    } else {
+      return (
+        <div className="app">
+          <div className='app-title-area'>
+            <h1 className='app-title'>The Traveling Fan</h1>
+          </div>
+          {/* <Nav /> */}
+          <div className="flex">
+          {console.log(this.state.nflTeams)}
+            <Container nflTeams={this.state.nflTeams} cities={cities} />
+          </div>
         </div>
-        {/* <Nav /> */}
-        <div className="flex">
-          <Container nflTeams={nflTeams} cities={cities} />
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/TeamIcon.js
+++ src/TeamIcon.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './reset.css';
 import './TeamIcon.scss';
 
-export default function teamIcon() {
+export default function teamIcon(props) {
 
     function toggleIconFunctions() {
-      this.props.toggleCardView();
-      this.props.grabIndex(this.props.index)
+      props.toggleCardView();
+      props.grabIndex(props.index)
     }
 
     return(
       <article onClick={toggleIconFunctions}>
-        <img src={this.props.nflTeam.helmet_image} /> 
-        <h2>{this.props.nflTeam.name}</h2>
+        <img src={props.nflTeam.team_icon} /> 
+        <h2>{props.nflTeam.name}</h2>
       </article>
     )
   }
-}
+
 
